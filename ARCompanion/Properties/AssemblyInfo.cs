65eefe927f3d40580d578a33793049ca8c76a327
Raemien <39755457+Raemien@users.noreply.github.com>
2020-10-07 01:27:11 +1300

    Update old assembly attributes

--- ARCompanion/Properties/AssemblyInfo.cs
+++ ARCompanion/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XR Companion")]
+[assembly: AssemblyTitle("AR Companion")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XRCompanion")]
+[assembly: AssemblyProduct("ARCompanion")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
