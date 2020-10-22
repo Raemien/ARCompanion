7616f61c29289247aa7858b766dd3b0b3feb28a7
Raemien <39755457+Raemien@users.noreply.github.com>
2020-10-22 16:22:29 +1300

    Temporary adaptations for new non-multiplayer objects

--- ARCompanion/EnvironmentHider.cs
+++ ARCompanion/EnvironmentHider.cs
@@ -13,56 +13,63 @@ namespace ARCompanion
         {
             if (hiddenObjs < 65536 || forceHide) // Tacky workaround for onSceneLoaded events not reliably triggering with environments
             {
-                var config = Settings.instance;
-                const string obj_pplace_menu = "Wrapper/MenuPlayersPlace";
-                const string obj_pplace = "Environment/PlayersPlace";
-                const string obj_smoke = "Environment/BigSmokePS";
-                const string obj_chevron = "Environment/SpawnRotationChevron";
-                const string obj_arrows = "Wrapper/Arrows";
-                Scene curScene = SceneManager.GetActiveScene();
-                GameObject environmentRoot;
-                Renderer[] appendedRenderers = { };
-                Renderer[] excludedRenderers = { };
-
-                switch (curScene.name)
+                try
                 {
-                    case "GameCore":
-                        environmentRoot = GameObject.Find(obj_pplace).transform.parent.gameObject;
-                        foreach (var obj in curScene.GetRootGameObjects().Where(obj => obj.name.Contains("Ring")))
+                    var config = Settings.instance;
+                    const string obj_pplace_menu = "MenuEnvironment/DefaultEnvironment/PlayersPlace";
+                    const string obj_pplace = "Environment/PlayersPlace";
+                    const string obj_smoke = "Environment/BigSmokePS";
+                    const string obj_chevron = "Environment/SpawnRotationChevron";
+                    const string obj_menu = "MenuEnvironment";
+                    Scene curScene = SceneManager.GetActiveScene();
+                    GameObject environmentRoot;
+                    Renderer[] appendedRenderers = { };
+                    Renderer[] excludedRenderers = { };
+
+                    switch (curScene.name)
+                    {
+                        case "GameCore":
+                            environmentRoot = GameObject.Find(obj_pplace).transform.parent.gameObject;
+                            foreach (var obj in curScene.GetRootGameObjects().Where(obj => obj.name.Contains("Ring")))
+                            {
+                                appendedRenderers = appendedRenderers.Concat(obj.GetComponentsInChildren<Renderer>()).ToArray();
+                            }
+                            if (GameObject.Find(obj_smoke) != null)
+                            {
+                                excludedRenderers = excludedRenderers.AddRangeToArray(!config.HideFog ? GameObject.Find(obj_smoke).GetComponentsInChildren<Renderer>() : Array.Empty<Renderer>());
+                            }
+                            excludedRenderers = excludedRenderers.AddRangeToArray(!config.HidePlatform && !GameObject.Find(obj_chevron) ? GameObject.Find(obj_pplace).GetComponentsInChildren<Renderer>() : Array.Empty<Renderer>());
+                            break;
+                        case "MenuViewControllers":
+                            environmentRoot = GameObject.Find(obj_pplace_menu).transform.parent.parent.gameObject;
+                            appendedRenderers = Array.Empty<Renderer>();
+                            break;
+                        default:
+                            environmentRoot = null;
+                            break;
+                    }
+
+                    if (environmentRoot != null)
+                    {
+                        Renderer[] renderers = environmentRoot.GetComponentsInChildren<Renderer>();
+                        if (appendedRenderers.Length > 0)
                         {
-                            appendedRenderers = appendedRenderers.Concat(obj.GetComponentsInChildren<Renderer>()).ToArray();
+                            renderers = renderers.Concat(appendedRenderers).ToArray();
                         }
-                        if (GameObject.Find(obj_smoke) != null)
+                        renderers = renderers.Except(excludedRenderers).ToArray();
+                        bool hiddensetting = curScene.name == "MenuViewControllers" ? !config.HideMenuEnv : !config.HideGameEnv;
+
+                        foreach (Renderer renderer in renderers)
                         {
-                            excludedRenderers = excludedRenderers.AddRangeToArray(!config.HideFog ? GameObject.Find(obj_smoke).GetComponentsInChildren<Renderer>() : Array.Empty<Renderer>());
+                            renderer.enabled = hiddensetting || unHide;
+                            hiddenObjs += forceHide ? 1 : 0;
                         }
-                        excludedRenderers = excludedRenderers.AddRangeToArray(!config.HidePlatform && !GameObject.Find(obj_chevron) ? GameObject.Find(obj_pplace).GetComponentsInChildren<Renderer>() : Array.Empty<Renderer>());
-                        break;
-                    case "MenuViewControllers":
-                        environmentRoot = GameObject.Find(obj_pplace_menu).transform.parent.gameObject;
-                        appendedRenderers = GameObject.Find(obj_arrows).GetComponentsInChildren<Renderer>();
-                        excludedRenderers = !config.HidePlatform ? GameObject.Find(obj_pplace_menu).GetComponentsInChildren<Renderer>() : Array.Empty<Renderer>();
-                        break;
-                    default:
-                        environmentRoot = null;
-                        break;
+                    }
                 }
-
-                if (environmentRoot != null)
+                catch (Exception)
                 {
-                    Renderer[] renderers = environmentRoot.GetComponentsInChildren<Renderer>();
-                    if (appendedRenderers.Length > 0)
-                    {
-                        renderers = renderers.Concat(appendedRenderers).ToArray();
-                    }
-                    renderers = renderers.Except(excludedRenderers).ToArray();
-                    bool hiddensetting = curScene.name == "MenuViewControllers" ? !config.HideMenuEnv : !config.HideGameEnv;
 
-                    foreach (Renderer renderer in renderers)
-                    {
-                        renderer.enabled = hiddensetting || unHide;
-                        hiddenObjs += forceHide ? 1 : 0;
-                    }
+                    throw;
                 }
             }
         }
